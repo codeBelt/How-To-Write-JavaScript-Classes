129d4700c4e88029b26e39135c1e75a73fdb48a0
rsavian <rsavian@nerdery.com>
2014-09-04 10:02:55 -0500

    update comments.

--- src/assets/scripts/views/BaseView.js
+++ src/assets/scripts/views/BaseView.js
@@ -1,65 +1,80 @@
 define(function (require, exports, module) { // jshint ignore:line
     'use strict';
 
-    var BaseView = (function () {
+var BaseView = (function () {
 
-        function BaseView($element) {
-            if (($element instanceof jQuery) === true && $element.length === 0) {
-                // If a jQuery object was passed in and it doesn't have any items then stop.
-                return;
-            }
-
-            this.$element = $element;
-            this.isEnabled = false;
-
-            this.createChildren()
-                .setupHandlers()
-                .enable()
-                .layoutChildren();
+    function BaseView($element) {
+        if (($element instanceof jQuery) === true && $element.length === 0) {
+            // If a jQuery object was passed in and it doesn't have any items then exit.
+            return;
         }
 
-        BaseView.prototype.createChildren = function() {
-            return this;
-        };
+        this.$element = $element;
+        this.isEnabled = false;
 
-        BaseView.prototype.setupHandlers = function() {
-            return this;
-        };
+        this.init();
+    }
 
-        BaseView.prototype.layoutChildren = function() {
-            return this;
-        };
+    BaseView.prototype.init = function() {
+        // Kick off the view lifecycle.
+        this.createChildren()
+            .setupHandlers()
+            .enable()
+            .layoutChildren();
 
-        BaseView.prototype.enable = function() {
-            if (this.isEnabled === true) {
-                return this;
+        return this;
+    };
+
+    BaseView.prototype.createChildren = function() {
+        // Used to create any object and/or find references to child elements.
+        return this;
+    };
+
+    BaseView.prototype.setupHandlers = function() {
+        // Creates a class reference to an event handler method that is bound to the scope of the object.
+        return this;
+    };
+
+    BaseView.prototype.layoutChildren = function() {
+        // Set or call to update data in your view.
+        return this;
+    };
+
+    BaseView.prototype.enable = function() {
+        if (this.isEnabled === true) {
+            return this;
+        }
+        this.isEnabled = true;
+
+        // Add event listeners for your class.
+
+        return this;
+    };
+
+    BaseView.prototype.disable = function() {
+        if (!this.isEnabled === false) {
+            return this;
+        }
+        this.isEnabled = false;
+
+        // Remove event listeners for your class.
+
+        return this;
+    };
+
+    BaseView.prototype.destroy = function() {
+        this.disable();
+
+        // Nulls out all properties of the class to prevent memory leak.
+        for (var key in this) {
+            if (this.hasOwnProperty(key)) {
+                this[key] = null;
             }
-            this.isEnabled = true;
-            return this;
-        };
+        }
+    };
 
-        BaseView.prototype.disable = function() {
-            if (!this.isEnabled === false) {
-                return this;
-            }
-            this.isEnabled = false;
-
-            return this;
-        };
-
-        BaseView.prototype.destroy = function() {
-            this.disable();
-
-            // Nulls out all properties of the class to prevent memory leak.
-            for (var key in this) {
-                if (this.hasOwnProperty(key)) {
-                    this[key] = null;
-                }
-            }
-        };
-
-        return BaseView;
-    })();
+    return BaseView;
+})();
 
     module.exports = BaseView;
 
